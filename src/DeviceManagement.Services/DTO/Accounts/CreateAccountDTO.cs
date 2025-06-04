50e6ccaaca2e7dad2d4128176700ea43905dbedb
some1less <muracks.max@gmail.com>
2025-06-04 15:47:35 +0200

    Added regexes

--- src/DeviceManagement.Services/DTO/Accounts/CreateAccountDTO.cs
+++ src/DeviceManagement.Services/DTO/Accounts/CreateAccountDTO.cs
@@ -6,12 +6,15 @@ public class CreateAccountDTO
 {
     [Required]
     [MaxLength(50)]
+    [RegularExpression("^[A-Za-z].*$", 
+        ErrorMessage="Username must start with a letter!")]
     public string Username { get; set; }
     
     [Required]
     [MinLength(12)]
     [MaxLength(25)]
-    // [RegularExpression()]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$",
+        ErrorMessage = "Password must contain at least one small letter, one capital letter, one number and one symbol!")]
     public string Password { get; set; }
     
     [Required]
